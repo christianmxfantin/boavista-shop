b25db1f3c767df44d738f9cfd1098e2196741383
Christian <christianmxfantin@gmail.com>
2022-03-21 22:00:48 -0300

    Add more styles to Navbar

--- client/src/components/Icon.jsx
+++ client/src/components/Icon.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { SvgIcon } from "@mui/material";
+import BrokenImageIcon from "@mui/icons-material/BrokenImage";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
-const icons = [{ name: "ShoppingCartIcon", img: ShoppingCartIcon }];
+const icons = [
+  { name: "ShoppingCartIcon", img: <SvgIcon component={ShoppingCartIcon} /> },
+];
 
 const Icon = ({ name }) => {
   let icon = icons.find((icon) => icon.name === name);
-  console.log(icon);
 
-  return <>{icon.img.render}</>;
+  return <>{!icon ? <SvgIcon component={BrokenImageIcon} /> : icon.img}</>;
 };
 
 export default Icon;
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,34 +1,205 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import { AppBar, Button, Tab, Tabs, Toolbar } from "@mui/material";
-//import Icon from "../Icon";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import Icon from "../Icon";
+
+// const pages = ["products", "login", "cart"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 const Navbar = () => {
-  return (
-    <>
-      <AppBar sx={{ backgroundColor: "primary" }}>
-        <Toolbar>
-          <NavLink to="/">
-            <Tab label="Logo" sx={{ color: "#fff" }} />
-          </NavLink>
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
 
-          <Tabs textColor="inherit">
-            <NavLink to="/products" className="navbar-links">
-              <Tab label="Productos" sx={{ marginLeft: "auto" }} />
-            </NavLink>
-            <NavLink to="/login">
-              <Button variant="contained">Login</Button>
-              <Button variant="contained">Sing up</Button>
-            </NavLink>
-            <NavLink to="/cart" className="navbar-links">
-              {/* <Icon name="ShoppingCartIcon" /> */}
-              <ShoppingCartIcon />
-            </NavLink>
-          </Tabs>
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static" sx={{ backgroundColor: "primary" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+          >
+            <Link style={{ textDecoration: "none", color: "white" }} to="/">
+              LOGO
+            </Link>
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {/* {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">
+                    <Link
+                      style={{ textDecoration: "none", color: "white" }}
+                      to={`${page}`}
+                    >
+                      {page}
+                    </Link>
+                  </Typography>
+                </MenuItem>
+              ))} */}
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">
+                  <Link
+                    style={{ textDecoration: "none", color: "white" }}
+                    to="/products"
+                  >
+                    Productos
+                  </Link>
+                </Typography>
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">
+                  <Link
+                    style={{ textDecoration: "none", color: "white" }}
+                    to="/cart"
+                  >
+                    Carrito
+                  </Link>
+                </Typography>
+              </MenuItem>
+            </Menu>
+
+            {/* MENU DESKTOP   */}
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+          >
+            <Link style={{ textDecoration: "none", color: "white" }} to="/">
+              LOGO
+            </Link>
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {/* {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                <Link
+                  style={{ textDecoration: "none", color: "white" }}
+                  to={`${page}`}
+                >
+                  {page}
+                </Link>
+              </Button>
+            ))} */}
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: "white", display: "block" }}
+            >
+              <Link
+                style={{ textDecoration: "none", color: "white" }}
+                to="/products"
+              >
+                Productos
+              </Link>
+            </Button>
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: "white", display: "block" }}
+            >
+              <Link
+                style={{ textDecoration: "none", color: "white" }}
+                to="/cart"
+              >
+                <Icon name="ShoppingCartIcon" />
+              </Link>
+            </Button>
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
         </Toolbar>
-      </AppBar>
-    </>
+      </Container>
+    </AppBar>
   );
 };
 
