49bb8038999fa3c2bce4b0b217e809ce6ad74d3e
Christian <christianmxfantin@gmail.com>
2022-06-01 20:11:04 -0300

    Add styles to ProductItem page

--- client/src/components/products/ProductItem.jsx
+++ client/src/components/products/ProductItem.jsx
@@ -2,66 +2,67 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useTheme } from "@emotion/react";
 import {
+  Box,
   Button as ProductAddToCart,
-  Card as ProductCard,
-  CardContent as ProductCardContent,
-  CardMedia as ProductCardImage,
-  Container as ProductCardData,
-  Typography as ProductName,
-  Typography as ProductPrice,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
 } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import { Icon as CartIcon } from "../ui/Icon";
 import ProductImage from "../../images/product.jpg";
+
+const ProductCard = styled(Card)(({ theme }) => ({
+  borderRadius: `${theme.spacing(0.5)}`,
+}));
+
+const ProductCardImage = styled(CardMedia)(({ theme }) => ({
+  //styles
+}));
+
+const ProductCardContent = styled(CardContent)(({ theme }) => ({
+  padding: `${theme.spacing(1)} !important`,
+}));
+
+const ProductName = styled(Typography)(({ theme }) => ({
+  color: `${theme.palette.primary[500]}`,
+  fontWeight: 500,
+  "&:hover": {
+    color: `${theme.palette.secondary[500]}`,
+    fontWeight: 600,
+  },
+}));
+
+const ProductCardData = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-between",
+}));
+
+const ProductPrice = styled(Typography)(({ theme }) => ({
+  color: `${theme.palette.primary[500]}`,
+}));
 
 const ProductItem = ({ data, addToCart }) => {
   const theme = useTheme();
   let { id, name, price } = data;
 
   return (
-    <ProductCard
-      sx={{
-        borderRadius: `${theme.spacing(0.5)}`,
-      }}
-    >
+    <ProductCard>
       <Link to={`/products/${id}`}>
         <ProductCardImage
           component="img"
           alt="prueba"
-          height="140"
+          height="180"
           image={ProductImage}
-          sx={{ height: "180px" }}
         />
       </Link>
-      <ProductCardContent sx={{ padding: `${theme.spacing(1)} !important` }}>
-        <ProductName
-          gutterBottom
-          variant="h6"
-          component="div"
-          sx={{
-            color: `${theme.palette.primary[500]}`,
-            fontWeight: 500,
-            "&:hover": {
-              color: `${theme.palette.secondary[500]}`,
-              fontWeight: 600,
-            },
-          }}
-        >
+      <ProductCardContent>
+        <ProductName gutterBottom variant="h6" component="div">
           {name}
         </ProductName>
-        <ProductCardData
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            padding: "0px !important",
-          }}
-        >
-          <ProductPrice
-            variant="body1"
-            sx={{ color: `${theme.palette.primary[500]}` }}
-          >
-            $ {price}
-          </ProductPrice>
-
+        <ProductCardData>
+          <ProductPrice variant="body1">$ {price}</ProductPrice>
           <ProductAddToCart
             size="small"
             data={data}
