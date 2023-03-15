c2053a507826c7fa055e6374d7b80b359c3e18ae
Christian <christianmxfantin@gmail.com>
2023-03-14 21:52:58 -0300

    Add API calls in Billing component

--- client/src/components/checkout/Billing/Billing.jsx
+++ client/src/components/checkout/Billing/Billing.jsx
@@ -20,25 +20,58 @@ const Billing = () => {
   const theme = useTheme();
   const nameInput = useRef();
   const [edit, setEdit] = useState(false);
+  // const [changeSelect, setChangeSelect] = useState(false);
+
   const [provincias, setProvincias] = useState([]);
+  const [departamentos, setDepartamentos] = useState([]);
 
   useEffect(() => {
     fetch("https://apis.datos.gob.ar/georef/api/provincias")
       .then((response) => response.json())
       .then((data) => {
-        const nombresProvincias = data.provincias.map(
-          (provincia) => provincia.nombre
-        );
-        const nombresProvinciasOrdenados = nombresProvincias.sort((a, b) =>
-          a.localeCompare(b)
-        );
-        setProvincias(nombresProvinciasOrdenados);
+        const stateNames = data.provincias.map((provincia) => provincia.nombre);
+        const stateNamesOrdered = stateNames.sort((a, b) => a.localeCompare(b));
+        setProvincias(stateNamesOrdered);
       });
   }, []);
 
+  const handleStateChange = (e) => {
+    const provincia = e.target.value;
+    const maxResults = 5000;
+    fetch(
+      `https://apis.datos.gob.ar/georef/api/localidades?provincia=${provincia}&max=${maxResults}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        // const totalResults = data.metadata.total;
+        console.log(data);
+        const cityNames = data.localidades.map((localidades) => {
+          const nombre = localidades.nombre;
+          const pronouns = ["los", "de"];
+          const capitalized = nombre
+            .split(" ")
+            .map((word) =>
+              pronouns.includes(word.toLowerCase())
+                ? word.toLowerCase()
+                : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+            )
+            .join(" ");
+          return capitalized;
+        });
+        const cityNamesFiltered = cityNames.filter((nombre) => {
+          return nombre.toUpperCase() !== "CIUDAD DE BUENOS AIRES";
+        });
+        const cityNamesOrdered = cityNamesFiltered.sort((a, b) =>
+          a.localeCompare(b)
+        );
+        console.log(cityNamesOrdered);
+        setDepartamentos(cityNamesOrdered);
+      });
+  };
+
   const handleEdit = () => {
     setEdit(true);
-    console.log(nameInput);
+    // console.log(nameInput);
     nameInput.current.focus();
   };
 
@@ -60,25 +93,29 @@ const Billing = () => {
         />
         <SurnameInput disabled={!edit} placeholder="Apellidos" />
         <AddressInput disabled={!edit} placeholder="Dirección" />
-        {/* <div>
-          <select id="provincia">
-            {provincias.map((provincia) => (
-              <option value={provincia} key={provincia}>
-                {provincia}
-              </option>
-            ))}
-          </select>
-        </div> */}
-        <StateSelect disabled={!edit} defaultValue={1}>
-          <MenuItem value={1}>Seleccione una Provincia</MenuItem>
-          {provincias.map((provincia) => (
-            <MenuItem value={provincia} key={provincia}>
+        <StateSelect
+          disabled={!edit}
+          defaultValue={1}
+          onChange={handleStateChange}
+        >
+          <MenuItem disabled value={1}>
+            Seleccione una Provincia
+          </MenuItem>
+          {provincias.map((provincia, index) => (
+            <MenuItem value={provincia} key={index}>
               {provincia}
             </MenuItem>
           ))}
         </StateSelect>
         <CitySelect disabled={!edit} defaultValue={1}>
-          <MenuItem value={1}>Seleccione una Localidad</MenuItem>
+          <MenuItem disabled value={1}>
+            Seleccione una Localidad
+          </MenuItem>
+          {departamentos.map((departamento, index) => (
+            <MenuItem value={departamento} key={index}>
+              {departamento}
+            </MenuItem>
+          ))}
         </CitySelect>
         <PostalCodeInput disabled={!edit} placeholder="Código Postal" />
         <MailInput disabled={!edit} placeholder="Mail" />
